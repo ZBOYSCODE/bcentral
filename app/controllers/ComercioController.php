b1e15e3798797bcd76642d4e28dfde98c88ab083
Jorge Silva A <jorge.silva@zentagroup.com>
2016-05-12 18:07:09 -0300

    Serv afectado -> componente (javascript)

--- app/controllers/ComercioController.php
+++ app/controllers/ComercioController.php
@@ -14,7 +14,6 @@ class ComercioController extends ControllerBase
      * Default action. Set the public layout (layouts/public.volt)
      */
     public function indexAction()
-
     {   
         $pcView = 'servicio/servicios_home_page';
         
@@ -38,14 +37,16 @@ class ComercioController extends ControllerBase
         echo $this->view->render('theme_default', array('lmView'=>'menu/leftMenu', 'menuSel'=>'','pcView'=>$pcView, 'pcData'=>''));    
     }
 
-    public function conocimientoAction() {
+    public function conocimientoAction() 
+    {
 
         $pcView = 'servicio/servicios_base_conocimiento';
 
         echo $this->view->render('theme_default',array('lmView'=>'menu/leftMenu','menuSel'=>'','pcView'=>$pcView,'pcData'=>''));
     }
 
-    public function solicitudServicioAction($tipo) {
+    public function solicitudServicioAction($tipo) 
+    {
 
         $js = '';
         if($tipo == "tecnologia") {
@@ -127,15 +128,17 @@ class ComercioController extends ControllerBase
         echo $this->view->render('theme_default', array('lmView'=>'menu/leftMenu', 'menuSel'=>'evaluarSol','pcView'=>$pcView, 'pcData'=> $data, 'jsScript'=>$js));    
     }
 
-     public function documentoAction() {
+     public function documentoAction() 
+    {
         $pcView = 'servicio/servicios_ver_documento';
 
         $js = '';
 
         echo $this->view->render('theme_default', array('lmView'=>'menu/leftMenu', 'menuSel'=>'evaluarSol','pcView'=>$pcView, 'pcData'=>'', 'jsScript'=>$js));    
-     }
+    }
 
-     public function evaluarAtencionModalAction()  {
+     public function evaluarAtencionModalAction()     
+    {
 
         $like = $_POST['optionLike'];
         $toRend=$this->view->render('servicio/servicios_encuesta_modal', array("like"=>$like));
@@ -145,112 +148,6 @@ class ComercioController extends ControllerBase
 
         $this->mifaces->run();
 
-     }
-
-
-
-    private function getJsCatalogo() {
-        $jsScript = "
-            $('#btnTec').click(function() {
-                
-                $('.bcAplicaciones').removeClass('hidden');
-            });
-            
-            $('#btnServ').click(function() {
-                $('.sg').removeClass('hidden');
-                $('.os').addClass('hidden');
-
-            });
-
-            $('#btnOtros').click(function() {
-                $('.os').removeClass('hidden');
-                $('.sg').addClass('hidden');
-
-            });
-
-            $('#btnCerrar').click(function() {
-                $('.strap').addClass('hidden');
-                $('.defaultClass').removeClass('hidden');
-                $('#textInicio').removeClass('hidden');
-                $('#bcInicio').addClass('hidden');
-                $('.nsBoton-tecnologia').removeClass('hidden');
-                $('.bcTecnologia').addClass('hidden');
-                $('.nsBoton-aplicaciones').addClass('hidden');
-                $('.bcTecnologia').addClass('hidden');
-                $('.bcAplicaciones').addClass('hidden');    
-                $('.bcTecnologia').addClass('hidden');
-                $('.bcFormSolicitudes').addClass('hidden');
-                $('#textFormSolicitudes').addClass('hidden');
-                $('#textFormSolicitudes').html('');
-            });
-
-            $('.nsBoton').click(function(event) {
-                $('.strap').addClass('hidden');
-                if($(this).data('next') == 'tecnologia'){
-
-                    $('#textInicio').addClass('hidden');
-                    $('#bcInicio').removeClass('hidden');
-                    $('.nsBoton-tecnologia').removeClass('hidden');
-                    $('.bcTecnologia').removeClass('hidden');
-                }
-                if($(this).data('next') == 'aplicaciones'){
-                    $('.nsBoton-aplicaciones').removeClass('hidden');
-                    $('.bcTecnologia').removeClass('hidden');
-                    $('.bcAplicaciones').removeClass('hidden'); 
-                }
-
-                if($(this).data('next') == 'form-solicitudes'){
-                    $('#helptitle').addClass('hidden');
-                    $('.bcTecnologia').removeClass('hidden');
-                    $('.bcFormSolicitudes').removeClass('hidden');
-                    $('#textFormSolicitudes').removeClass('hidden');
-                    if($(this).data('type') == 'terceraVista'){
-                        $('#textFormSolicitudes').html($(this).data('val'));                
-                    } else if ($(this).data('type') == 'segundaVista') {
-                        $('#textFormSolicitudes').html($(this).data('val'));                        
-                    } else if ($(this).data('type') == 'primeraVista') {
-                        $('#bcInicio').removeClass('hidden');
-                        $('#textInicio').addClass('hidden');
-                        $('#textFormSolicitudes').html($(this).data('val'));
-                    }
-                    $('#infoTitle').html('Información Específica');
-                    $('.nsBoton-form-solicitudes').removeClass('hidden');   
-                }
-            });
-            
-        ";
-
-        return $jsScript;
-    }
-
-    private function getJsDatatables() {
-        $jsScript =
-        "
-            var TablesDatatables = function() {
-
-                return {
-                    init: function() {
-                        /* Initialize Bootstrap Datatables Integration */
-                        //App.datatables();
-
-                        /* Initialize Datatables */
-                        $('#table').dataTable({
-                            columnDefs: [ { orderable: true, targets: [ 1, 2, 3] } ]
-                        });
-
-                        /* Add placeholder attribute to the search input */
-                        $('div.dataTables_length').html('');
-                        $('#table_filter').html('');
-                        $('#table_info').html('');
-                        $('#table_paginate').html('');
-                    }
-                };
-            }();
-
-            $(function(){ TablesDatatables.init(); });
-        ";
-
-        return $jsScript;
     }
 
 
@@ -333,40 +230,11 @@ class ComercioController extends ControllerBase
         //echo '<br/><br/>Request : <br/><xmp>'. $response['request'] . '</xmp>';
     }
 
-    private function getLikeJs() {
-        $jsScript =
-        "
-            $(\"[name^='conf']\").on('click', function() {
-            $(\"#optionLike\").val($(this).val());
-            $(\"#ticketId\").val('SD1234');
-            $(\"#evaluacionForm\").submit();
-            });
-
-
-        ";
-    
-        return $jsScript;
-    }
-
-    private function getLikeEvalJs() {
-        $jsScript =
-        "
-            $(\"[name^='preg0']\").on('click', function() {
-                $(\"#preg01\").toggleClass('likeimgGray');
-                $(\"#preg01\").toggleClass('likeimg');
-                $(\"#preg02\").toggleClass('dislikeimgGray');
-                $(\"#preg02\").toggleClass('dislikeimg');
-            });
-        ";
-
-        return $jsScript;
-    }
-
     public function TestCreateInteractionAction()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'GET') 
         {
-            $campos = array(
+           /* $campos = array(
                     'detinatario' => 'si',
                     'ci' => 'si',
                     'titulo' => 'si',
@@ -389,9 +257,197 @@ class ComercioController extends ControllerBase
 
             $ciItem = new CI();
             $ciList = $ciItem->getCompleteCIList();
+            */
             $pcView = 'servicio/servicios_solicitud_test';
-            $data = array('campos' => $campos, 'catalog' => $catalog, 'contactos' => $contactList, 'ci' => $ciList);
-            echo $this->view->render('theme_default', array('lmView'=>'menu/leftMenu', 'menuSel'=>'','pcView'=>$pcView, 'pcData'=> $data));
+            //$data = array('campos' => $campos, 'catalog' => $catalog, 'contactos' => $contactList, 'ci' => $ciList);
+
+            //asi debe ser la lista de  servicios afectados y sus hijos ci
+            $listas = array('servicio afectado 1' => array('ci1','ci2','ci3'),
+                            'servicio afectado 2' => array('ci4','ci5','ci6'),
+                            'servicio afectado 3' => array('ci7','ci8','ci9'));
+
+            $data['listas'] = $listas;
+
+            $js = $this->getComponenteServAfectadoJs($listas);
+
+            echo $this->view->render('theme_default', array('lmView'=>'menu/leftMenu', 'menuSel'=>'','pcView'=>$pcView, 'pcData'=> $data, 'jsScript'=>$js));
         }
     }
+
+
+
+
+
+    /* 
+        Métodos para cargar Javascript 
+    */
+
+    private function getJsCatalogo() 
+    {
+        $jsScript = "
+            $('#btnTec').click(function() {
+                
+                $('.bcAplicaciones').removeClass('hidden');
+            });
+            
+            $('#btnServ').click(function() {
+                $('.sg').removeClass('hidden');
+                $('.os').addClass('hidden');
+
+            });
+
+            $('#btnOtros').click(function() {
+                $('.os').removeClass('hidden');
+                $('.sg').addClass('hidden');
+
+            });
+
+            $('#btnCerrar').click(function() {
+                $('.strap').addClass('hidden');
+                $('.defaultClass').removeClass('hidden');
+                $('#textInicio').removeClass('hidden');
+                $('#bcInicio').addClass('hidden');
+                $('.nsBoton-tecnologia').removeClass('hidden');
+                $('.bcTecnologia').addClass('hidden');
+                $('.nsBoton-aplicaciones').addClass('hidden');
+                $('.bcTecnologia').addClass('hidden');
+                $('.bcAplicaciones').addClass('hidden');    
+                $('.bcTecnologia').addClass('hidden');
+                $('.bcFormSolicitudes').addClass('hidden');
+                $('#textFormSolicitudes').addClass('hidden');
+                $('#textFormSolicitudes').html('');
+            });
+
+            $('.nsBoton').click(function(event) {
+                $('.strap').addClass('hidden');
+                if($(this).data('next') == 'tecnologia'){
+
+                    $('#textInicio').addClass('hidden');
+                    $('#bcInicio').removeClass('hidden');
+                    $('.nsBoton-tecnologia').removeClass('hidden');
+                    $('.bcTecnologia').removeClass('hidden');
+                }
+                if($(this).data('next') == 'aplicaciones'){
+                    $('.nsBoton-aplicaciones').removeClass('hidden');
+                    $('.bcTecnologia').removeClass('hidden');
+                    $('.bcAplicaciones').removeClass('hidden'); 
+                }
+
+                if($(this).data('next') == 'form-solicitudes'){
+                    $('#helptitle').addClass('hidden');
+                    $('.bcTecnologia').removeClass('hidden');
+                    $('.bcFormSolicitudes').removeClass('hidden');
+                    $('#textFormSolicitudes').removeClass('hidden');
+                    if($(this).data('type') == 'terceraVista'){
+                        $('#textFormSolicitudes').html($(this).data('val'));                
+                    } else if ($(this).data('type') == 'segundaVista') {
+                        $('#textFormSolicitudes').html($(this).data('val'));                        
+                    } else if ($(this).data('type') == 'primeraVista') {
+                        $('#bcInicio').removeClass('hidden');
+                        $('#textInicio').addClass('hidden');
+                        $('#textFormSolicitudes').html($(this).data('val'));
+                    }
+                    $('#infoTitle').html('Información Específica');
+                    $('.nsBoton-form-solicitudes').removeClass('hidden');   
+                }
+            });
+            
+        ";
+
+        return $jsScript;
+    }
+
+    private function getJsDatatables() 
+    {
+        $jsScript =
+        "
+            var TablesDatatables = function() {
+
+                return {
+                    init: function() {
+                        /* Initialize Bootstrap Datatables Integration */
+                        //App.datatables();
+
+                        /* Initialize Datatables */
+                        $('#table').dataTable({
+                            columnDefs: [ { orderable: true, targets: [ 1, 2, 3] } ]
+                        });
+
+                        /* Add placeholder attribute to the search input */
+                        $('div.dataTables_length').html('');
+                        $('#table_filter').html('');
+                        $('#table_info').html('');
+                        $('#table_paginate').html('');
+                    }
+                };
+            }();
+
+            $(function(){ TablesDatatables.init(); });
+        ";
+
+        return $jsScript;
+    }
+
+    private function getLikeJs() 
+    {
+        $jsScript =
+        "
+            $(\"[name^='conf']\").on('click', function() {
+            $(\"#optionLike\").val($(this).val());
+            $(\"#ticketId\").val('SD1234');
+            $(\"#evaluacionForm\").submit();
+            });
+
+
+        ";
+    
+        return $jsScript;
+    }
+
+    private function getLikeEvalJs()
+    {
+        $jsScript =
+        "
+            $(\"[name^='preg0']\").on('click', function() {
+                $(\"#preg01\").toggleClass('likeimgGray');
+                $(\"#preg01\").toggleClass('likeimg');
+                $(\"#preg02\").toggleClass('dislikeimgGray');
+                $(\"#preg02\").toggleClass('dislikeimg');
+            });
+        ";
+
+        return $jsScript;
+    }
+
+    private function getComponenteServAfectadoJs($lista)
+    {
+
+        //lista debe venir de la forma Array("padre"=>array("hijo1","hijo2",...),"padre2"=>array(...));
+        $listajson = json_encode($lista);
+
+        $jsScript =
+        "
+        $(\"#select_sa\").on('change', function(){
+                //lista completa desde ws
+                var lista_completa =".$listajson." ;
+
+                //dejamos vacia la listaASasa
+                $(\"#select_ci\").empty();
+                $(\"#select_ci\").trigger(\"chosen:updated\");
+
+                // actualizamos el select de componente
+                $.each(lista_completa[$(this).val()], function(key,value) {
+                
+                  var option = $(\"<option></option>\")
+                    .attr(\"value\", value)
+                    .text(value);
+                  $(\"#select_ci\").append(option);
+                });
+                $(\"#select_ci\").trigger(\"chosen:updated\");
+        });
+        ";
+
+        return $jsScript;
+    }
+
 }
