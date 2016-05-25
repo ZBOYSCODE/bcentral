80663c3159e8f9d74180691fa5c8bdc630a2ee3a
jorge cocina <jorge.cocina@zentagroup.com>
2016-05-25 14:17:56 -0400

    asdad as da d

--- app/controllers/ComercioController.php
+++ app/controllers/ComercioController.php
@@ -78,20 +78,31 @@ class ComercioController extends ControllerBase
             $response = new \Phalcon\Http\Response();
             return $response->redirect("");
         }
-        $ctlg = new Catalog();
-        $catalogoMenu = $ctlg->getServiceCatalogSP1($catalogoPadre);
+        try
+        {
+            $ctlg = new Catalog();
+            $catalogoMenu = $ctlg->getServiceCatalogSP1($catalogoPadre);
 
-        $pcData['catalogo'] = $catalogoMenu;
-        $pcData['styleCssMenu'] = $styleCssMenu;
-        $pcData['catalogoRutaCompleta'] = $catalogoPadre;
+            $pcData['catalogo'] = $catalogoMenu;
+            $pcData['styleCssMenu'] = $styleCssMenu;
+            $pcData['catalogoRutaCompleta'] = $catalogoPadre;
 
-        //breadcrum de la siguiente forma: "opcion","active" (con hipervinculo o no), "url" (relativa)
-        $pcData['breadCrumbList']  = [
-          array($catalogoPadre,'active',$tipo),
-        ];
+            //breadcrum de la siguiente forma: "opcion","active" (con hipervinculo o no), "url" (relativa)
+            $pcData['breadCrumbList']  = [
+              array($catalogoPadre,'active',$tipo),
+            ];
 
-        $js = $this->getJsCatalogo();
-        echo $this->view->render('theme_default',array('lmView'=>'menu/leftMenu','menuSel'=>'','pcView'=>$pcView,'pcData'=>$pcData,'jsScript'=>$js));
+            $js = $this->getJsCatalogo();
+            echo $this->view->render('theme_default',array('lmView'=>'menu/leftMenu','menuSel'=>'','pcView'=>$pcView,'pcData'=>$pcData,'jsScript'=>$js));
+        }
+        catch (Exception $e)
+        {
+            $js = "$.bootstrapGrowl('Error Interno. Repita el procedimiento.',{type:\"warning\",align:\"center\"});";
+            $pcView = 'servicio/servicios_error_page';
+            $data = array( 'error-number' => '500 - Error interno en el servidor', 'error-description' => 'Problemas al establecer conexión a los web service, por favor revisar permisos de acceso y configuración.' );
+            echo $this->view->render('theme_default',array('lmView'=>'menu/leftMenu','menuSel'=>'','pcView'=>$pcView,'pcData'=>$pcData,'jsScript'=>$js));
+        }
+        
 
     }
 
